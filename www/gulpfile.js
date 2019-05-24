551b5829b0c07e8ccbc7e4391b5926a16b8998a2
Ondřej Vraštil <vrastilondrej@gmail.com>
2019-05-25 00:58:02 +0200

    edit gitignore and gulp files

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -9,33 +9,33 @@ var browserSync = require('browser-sync').create();
 
 // Static server
 gulp.task('start', function () {
-    browserSync.init({
-        proxy: "jinci.test"
-    });
+   browserSync.init({
+      proxy: "jincicin.test"
+   });
 
-    gulp.watch("./user/themes/jinci-cin/css/*.css", ['tailwind', browserSync.reload]);
-    gulp.watch("./user/themes/jinci-cin/templates/**/*.*").on('change', browserSync.reload);
-        gulp.watch("./tailwind.js").on('change', browserSync.reload);
-            gulp.watch("./tailwind.js", ['tailwind']);
+   gulp.watch("./user/themes/jinci-cin/css/*.css", ['tailwind', browserSync.reload]);
+   gulp.watch("./user/themes/jinci-cin/templates/**/*.*").on('change', browserSync.reload);
+   gulp.watch("./tailwind.js").on('change', browserSync.reload);
+   gulp.watch("./tailwind.js", ['tailwind']);
 
 
 });
 
 
 gulp.task('tailwind', function () {
-    return gulp.src('./user/themes/jinci-cin/css/custom.css')
-        .pipe(postcss([
-            tailwindcss('./tailwind.js'),
-            autoprefixer,
-        ]))
-        .on('error', swallowError)
-        .pipe(gulp.dest('./user/themes/jinci-cin/css/public/'))
+   return gulp.src('./user/themes/jinci-cin/css/custom.css')
+      .pipe(postcss([
+         tailwindcss('./tailwind.js'),
+         autoprefixer,
+      ]))
+      .on('error', swallowError)
+      .pipe(gulp.dest('./user/themes/jinci-cin/css/public/'))
 })
 
 gulp.task('default', ['start']);
 
 function swallowError(error) {
-    // If you want details of the error in the console
-    console.log(error.toString())
-    this.emit('end')
+   // If you want details of the error in the console
+   console.log(error.toString())
+   this.emit('end')
 }
